fb14b1411b5951079cfad9f7b1b5579c9ea6af7d
randyisalive <99945506+randyisalive@users.noreply.github.com>
2022-12-12 13:39:22 +0700

    fix center login

--- login.php
+++ login.php
@@ -7,28 +7,35 @@
     <!-- INCLUDE NAVBAR HERE -->
     <?php include('navbar.php') ?>
     <!-- INCLUDE NAVBAR HERE -->
-
-    <div class="container mt-5">
-        <form action="process_login.php" class="form-control" method="post">
+    <div class="row justify-content-center">
+        <div class="col-6">
             <div class="container mt-5">
-                <h1 class="text-center">Login Page</h1>
-            </div>
-            <div class="container">
-                <div class="container-fluid mt-3">
-                    <span class="p">Username: </span>
-                    <input type="text" class="form-control" name="username" required />
-                </div>
-                <div class="container-fluid mt-3">
-                    <span class="p mt-3">Password: </span>
-                    <input type="password" name="password" class="form-control" required />
-                </div>
+                <form action="process_login.php" class="form-control" method="post">
+                    <div class="container mt-5">
+                        <h1 class="text-center">Login Page</h1>
+                    </div>
+                    <div class="container">
+                        <div class="container-fluid mt-3">
+                            <span class="p">Username: </span>
+                            <input type="text" class="form-control" name="username" required />
+                        </div>
+                        <div class="container-fluid mt-3">
+                            <span class="p mt-3">Password: </span>
+                            <input type="password" name="password" class="form-control" required />
+                        </div>
 
-                <div class="text-center mt-3">
-                    <input type="submit" name="submit" class="btn btn-primary" value="Login" />
-                </div>
+                        <div class="text-center mt-3">
+                            <input type="submit" name="submit" class="btn btn-primary" value="Login" />
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
+
+        </div>
+
     </div>
+
+
     <?php include('footer.php') ?>
 </body>
 

3054f75e9e8f8805d6148b17a904fc7607b98dbc
randyisalive <99945506+randyisalive@users.noreply.github.com>
2022-12-15 20:09:33 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2022 at 06:59 AM
+-- Generation Time: Dec 15, 2022 at 02:09 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -14,46 +14,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `payroll_website`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `department`
---
-
-CREATE TABLE `department` (
-  `id` int(15) NOT NULL,
-  `department` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `department`
---
-
-INSERT INTO `department` (`id`, `department`) VALUES
-(1, 'IT Consultant'),
-(2, 'Public Relations');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `saved`
---
-
-CREATE TABLE `saved` (
-  `id` int(15) NOT NULL,
-  `users_id` int(15) NOT NULL,
-  `salary` varchar(255) NOT NULL,
-  `time_saved` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `saved`
---
-
-INSERT INTO `saved` (`id`, `users_id`, `salary`, `time_saved`) VALUES
-(4, 1, '105589.44', '2022-12-15'),
-(7, 2, '696833.28', '2022-12-15');
 
 -- --------------------------------------------------------
 
@@ -78,27 +38,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `profile_picture`, `age`, `department`, `salary`, `FullName`) VALUES
-(1, 'rei', 'rei', 'rei@gmail.com', 'Test_image.jpg', '20', 'IT Consultant', '105589.44', 'Rahmandi Fitra Madenda'),
-(2, 'admin', 'admin', 'admin@gmail.com', 'admin.jpg', '21', 'IT Consultant', '696833.28', 'admin');
+(1, 'Rahmandi', 'rei', 'rei@gmail.com', 'Test_image.jpg', '20', 'IT Consultant', '9456.48', 'RAHMANDI FITRA MADENDA'),
+(2, 'admin', 'admin', 'admin', 'admin.jpg', '20', 'Public Relations', '696833.28', 'RoyalColony');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `department` (`department`);
-
---
--- Indexes for table `saved`
---
-ALTER TABLE `saved`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_id` (`users_id`),
-  ADD KEY `salary` (`salary`);
 
 --
 -- Indexes for table `users`
@@ -113,37 +58,18 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `department`
---
-ALTER TABLE `department`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `saved`
---
-ALTER TABLE `saved`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `saved`
---
-ALTER TABLE `saved`
-  ADD CONSTRAINT `saved_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`department`) REFERENCES `department` (`department`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`salary`) REFERENCES `saved` (`salary`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`department`) REFERENCES `department` (`department`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
